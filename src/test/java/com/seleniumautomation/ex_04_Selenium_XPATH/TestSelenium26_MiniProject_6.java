1acbe7cb83421dabf296555f356b3c57ff24ed5e
rahkumar <rahulkumaraiet@yahoo.in>
2025-05-20 18:19:33 +0530

    20 May 2025

--- src/test/java/com/seleniumautomation/ex_04_Selenium_XPATH/TestSelenium26_MiniProject_6.java
+++ src/test/java/com/seleniumautomation/ex_04_Selenium_XPATH/TestSelenium26_MiniProject_6.java
@@ -36,19 +36,53 @@ public class TestSelenium26_MiniProject_6 {
 
         Thread.sleep(2000);
 
-        WebElement table = driver.findElement(By.xpath("//div[@role ='table']/div[@class='oxd-table-body']"));
+        //WebElement table = driver.findElement(By.xpath("//div[@role ='table']/div[@class='oxd-table-body']"));
 //       JavascriptExecutor js = (JavascriptExecutor) driver;
 //       js.executeScript("arguments[100].scrollIntoView(true);", table);
 
-        List<WebElement> row = table.findElements(By.className("oxd-table-card"));
-        for (int i = 0; i < row.size(); i++) {
-            List<WebElement> col = row.get(i).findElements(By.xpath(""));
-            for (WebElement c: col) {
 
-                System.out.println(c.getText());
+        //first part -//div[@role ='table']/div[@class='oxd-table-body']/div[@class='oxd-table-card'][
+        //i
+        // ]//div[@role='cell'][
+        //j
+        // ]
+
+        String first_part = "//div[@role ='table']/div[@class='oxd-table-body']/div[@class='oxd-table-card'][";
+        String second_part = "]//div[@role='cell'][";
+        String third_part = "]";
+
+        int row = driver.findElements((By.className("oxd-table-card"))).size();
+        int col = driver.findElements((By.xpath("//div[@role='cell']"))).size();
+
+        for (int i = 2; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                String dyanmic_path = first_part+i+second_part+j+third_part;
+                System.out.println(dyanmic_path);
+                String data = driver.findElement(By.xpath(dyanmic_path)).getText();
+               // System.out.println(data);
+                if (data.contains("Terminated")){
+                    String emp_status = dyanmic_path+"/following-sibling::td";
+//                    String country_text  = driver.findElement(By.xpath(country_path)).getText();
+//                    System.out.println("--------------");
+//                    System.out.println("Helen Bennett is In "+country_text);
+
+                }
 
             }
+
         }
+
+
+//        List<WebElement> row = findElements(By.className("oxd-table-card"));
+//        for (int i = 0; i < row.size(); i++) {
+//            List<WebElement> col = row.get(i).findElements(By.xpath("//div[@role='cell']"));
+//            for (WebElement c: col) {
+//
+//                System.out.println(c.getText());
+//
+//
+//            }
+//        }
 
 
 
